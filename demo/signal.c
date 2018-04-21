60a3c672f29989e97ea914bf1f8fdd74edf56201
ljw <nicekingwei@sina.com>
2018-04-21 16:11:10 +0800

    signal

--- demo/signal.c
+++ demo/signal.c
@@ -1,3 +1,11 @@
+/*
+ * signal
+ * 运行上面的程序 在另一个终端中输入
+ * $ pgrep -l test		（获取进程test的pid）
+ * 7816 test
+ * $ kill -s SIGUSR1 7816		（发送信号）
+ * 则会执行signal_handle函数
+ */
 #include <stdio.h>  
 #include <signal.h>  
 #include <unistd.h>
@@ -17,9 +25,9 @@ int main(int argc,char **argv)
     while(1)   
     {  
         sleep(1); putchar('1'); fflush(stdout);
-	sleep(1); putchar('2'); fflush(stdout);
-	sleep(1); putchar('3'); fflush(stdout);
-	sleep(1); putchar('4'); fflush(stdout);
+        sleep(1); putchar('2'); fflush(stdout);
+        sleep(1); putchar('3'); fflush(stdout);
+        sleep(1); putchar('4'); fflush(stdout);
     }  
     return 0 ;  
 }
