20ab6d73f4f581fc2889fa766ea0f147c617285e
nicekingwei <nicekingwei@foxmail.com>
2018-05-19 18:39:25 +0800

    fix 64 demo

--- demo/bench1_patch_64.c
+++ demo/bench1_patch_64.c
@@ -8,13 +8,18 @@ void fix_init(int* p_global_data) {
     printf("fix_init");
 }
 
-int fix_is_prime(int n) {
-    *global_data++;
+__asm__(
+    ".global fix_is_prime\n"
+    "fix_is_prime:\n"
+    "pop %rax\n\t"
+    "jmp is_prime\n\t"
+);
+static int is_prime(int n) {
+    (*global_data)+=2;
     global_y++;
-    return 0;
-    printf("fix\n");
     for(int i=2;i*i<n;i++){
         if(n%i==0) return 0;
     }
+    printf("fix\n");
     return 1;
 }
